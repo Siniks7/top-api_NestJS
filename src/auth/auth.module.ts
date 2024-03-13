a86f9585b97daa34480626132616b937f665067b
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-03-13 23:51:04 +0700

    переменные окружения

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,10 @@
 /* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
 
-@Module({})
+@Module({
+  controllers: [AuthController]
+})
 export class AuthModule {
 }
 

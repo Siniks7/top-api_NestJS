35ffaae64864c4181eff0d31fd3b117662d3b095
Nikolay Sponarovskiy <siniks7@yandex.ru>
2024-03-16 20:24:14 +0700

    fix 2

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -1,17 +1,17 @@
-import { ConfigService } from '@nestjs/config'
-import { TypegooseModuleOptions } from 'nestjs-typegoose'
+import { ConfigService } from '@nestjs/config';
+import { TypegooseModuleOptions } from 'nestjs-typegoose';
 
 export const getMongoConfig = async (
-  configService: ConfigService,
+	configService: ConfigService
 ): Promise<TypegooseModuleOptions> => {
-  return {
-    uri: getMongoString(configService),
-    ...getMongoOptions(),
-  }
-}
+	return {
+		uri: getMongoString(configService),
+		...getMongoOptions()
+	};
+};
 
 const getMongoString = (configService: ConfigService) =>
-  'mongodb://' +
+	'mongodb://' +
   configService.get('MONGO_LOGIN') +
   ':' +
   configService.get('MONGO_PASSWORD') +
@@ -21,10 +21,10 @@ const getMongoString = (configService: ConfigService) =>
   configService.get('MONGO_PORT') +
   '/' +
   configService.get('MONGO_AUTHDATABASE') +
-  '?directConnection=true'
+  '?directConnection=true';
 
 const getMongoOptions = () => ({
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useUnifiedTopology: true,
-})
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true
+});
